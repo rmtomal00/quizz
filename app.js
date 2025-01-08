b21c96a4178a134a59e1987a910742236370e16d
rmtomal <rmtomal11@gmail.com>
2025-01-08 16:23:22 +0600

    init

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const quizeController = require('./controller/quizeController')
+const path = re
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
